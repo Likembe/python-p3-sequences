dea4b6ca4c5ac06fbf37819d18ab5b1e03189896
Alpha <alpha.likembe@student.moringaschool.com>
2023-09-30 09:16:54 +0300

    practice

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+
+    if length == 0:
+        print(fibonacci_sequence)
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        a, b = 0, 1
+        fibonacci_sequence.extend([a, b])
+
+        while len(fibonacci_sequence) < length:
+            next_number = a + b
+            fibonacci_sequence.append(next_number)
+            a, b = b, next_number
+
+        print(fibonacci_sequence)
+
+# Example usage:
+print_fibonacci(0)  # Prints an empty list
+print_fibonacci(1)  # Prints [0]
+print_fibonacci(10)  # Prints [0, 1, 1, 2, 3]
